a6c527261423029ad241e61e02e66b12b898f8ec
Micah <airrenno@yahoo.com>
2022-11-15 21:29:02 -0600

    Update robot and Dinosaur output statements.

--- dinosaur.py
+++ dinosaur.py
@@ -10,12 +10,12 @@ class Dinosaur:
 
     def __init__(self, name, attack_power):
         self.name = name
-        self.health = 100
+        self.health = 600
         self.attack_power = attack_power
 
     def dinosaur_attack(self, robot):
         print(f"{self.name} attacks {robot.name} causing {self.attack_power} damage!")
-        robot_life =  robot.health - self.attack_power
+        robot_life = robot.health - self.attack_power
         robot.health = robot_life
         print(f"{robot.name} health is now {robot_life}!")
         print()
